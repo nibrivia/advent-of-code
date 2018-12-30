7ef4bf8d2dbfadd1f6aa6c65682af6b7b78d3cce
Olivia Brode-Roger <nibr@mit.edu>
2018-12-29 20:39:48 -0500

    Day 3, part two

--- day-3-no-matter-how-you-slice-it.py
+++ day-3-no-matter-how-you-slice-it.py
@@ -10,12 +10,22 @@ claims = parse_claims(claims_str)
 
 # Part one
 board = dict()
+conflict_free = set(claim_id for claim_id, _, _, _, _ in claims)
 
 for claim_id, x, y, width, height in claims:
     for dx in range(width):
         for dy in range(height):
             position = (x+dx, y+dy)
-            board[position] = board.get(position, 0) + 1
 
-contested = [c for c in board.values() if c > 1]
+            # Add claim to the board
+            current_claims = board.get(position, set())
+            current_claims.add(claim_id)
+            board[position] = current_claims
+
+            if len(current_claims) >= 2:
+                conflict_free -= current_claims
+
+contested = [c for c in board.values() if len(c) > 1]
 print(len(contested))
+
+print(conflict_free)
